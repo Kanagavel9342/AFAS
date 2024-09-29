3a285709555d9be494133335c83865390427f96b
Karthick subramanian <karthicksubbiramaniyan@gmail.com>
2024-09-29 10:13:33 +0530

    Update Loginback.java

--- Loginback.java
+++ Loginback.java
@@ -5,16 +5,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/submit")
+@WebServlet("/Loginback") // Make sure this URL matches the form action
 public class FormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Retrieve username and password from the form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-         if ("9923017046".equals(username) && "BCA".equals(password)) {
-            // Success case - Redirect to the dashboard
+
+        // Simple authentication logic (you can replace this with database authentication)
+        if ("9923017046".equals(username) && "BCA".equals(password)) {
+            // Successful login - redirect to the dashboard page
             response.sendRedirect("dash.html");
         } else {
+            // Failed login - redirect back to the login page with an error message
             response.sendRedirect("login.html?error=invalid");
         }
     }
