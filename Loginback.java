ff264533d15fc9671f5423f98e608ae6bec54e82
Karthick subramanian <karthicksubbiramaniyan@gmail.com>
2024-10-07 16:59:25 +0530

    Update Loginback.java

--- Loginback.java
+++ Loginback.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet("/Loginback")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
